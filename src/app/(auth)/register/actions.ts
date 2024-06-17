e8921c93e6ee9e8334af3e246a7254d0ea5c4c7e
Franco Campaiola <francocampaiola@hotmail.com>
2024-06-17 00:07:08 -0300

    fix: ajustes en la creación del usr

--- src/app/(auth)/register/actions.ts
+++ src/app/(auth)/register/actions.ts
@@ -2,24 +2,45 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-
 import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers';
+import { cookies } from 'next/headers'
 
 export async function signup(formData: FormData) {
     const cookiesStore = cookies()
     const supabase = createClient(cookiesStore)
-    const data = {
-        email: formData.get('email') as string,
-        password: formData.get('password') as string
-    }
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
 
-    const { error } = await supabase.auth.signUp(data)
+    const { data: signUpData, error: signUpError } = await supabase.auth.signUp({ email, password })
 
-    if (error) {
+    if (signUpError) {
+        console.error('Error during sign up:', signUpError)
         redirect('/error')
     }
 
+    const user = signUpData?.user
+    if (!user) {
+        console.error('No user data returned from sign up')
+        redirect('/error')
+    }
+
+    const { error: insertError } = await supabase
+        .from('users')
+        .insert({
+            fk_user: user.id,
+            email: email,
+            fk_role: 3,
+            is_active: true,
+            created_at: new Date().toISOString()
+        })
+        .single()
+
+    if (insertError) {
+        console.error('Error during inserting user data:', insertError)
+        redirect('/error')
+    }
+
+    console.log('User data inserted successfully:', user.id)
     revalidatePath('/', 'layout')
     redirect('/')
 }
