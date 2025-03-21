85de5698971e2bdc5d465379aec07335be2afecc
Franco Campaiola <francocampaiola@hotmail.com>
2025-03-21 11:02:07 -0300

    Eliminar archivo de callback de MercadoPago y actualizar la lógica de redirección en la API para utilizar una nueva URL de conexión, simplificando así el manejo de autorizaciones.

--- src/api.ts
+++ src/api.ts
@@ -5,31 +5,21 @@ export const mercadopago = new MercadoPagoConfig({ accessToken: process.env.MP_A
 const api = {
   user: {
     async authorize() {
-      // Determinar la URL de redirección según el entorno
-      const redirectUri = process.env.NODE_ENV === 'development' 
-        ? 'http://localhost:3000/api/mercadopago/callback'
-        : `${process.env.NEXT_PUBLIC_APP_URL}/api/mercadopago/callback`;
-
       const url = new OAuth(mercadopago).getAuthorizationURL({
         options: {
           client_id: process.env.NEXT_PUBLIC_MP_CLIENT_ID,
-          redirect_uri: redirectUri
+          redirect_uri: `${process.env.NEXT_PUBLIC_APP_URL}/api/mercadopago/connect`
         }
       })
       return url
     },
     async connect(code: string) {
-      // Determinar la URL de redirección según el entorno
-      const redirectUri = process.env.NODE_ENV === 'development' 
-        ? 'http://localhost:3000/api/mercadopago/callback'
-        : `${process.env.NEXT_PUBLIC_APP_URL}/api/mercadopago/callback`;
-
       const credentials = await new OAuth(mercadopago).create({
         body: {
           client_id: process.env.NEXT_PUBLIC_MP_CLIENT_ID,
           client_secret: process.env.MP_CLIENT_SECRET,
           code,
-          redirect_uri: redirectUri
+          redirect_uri: `${process.env.NEXT_PUBLIC_APP_URL}/api/mercadopago/connect`
         }
       })
       return credentials
