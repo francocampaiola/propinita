3f954dca64cafe66c948e6b0166a9bb784cdabea
Franco Campaiola <francocampaiola@hotmail.com>
2025-05-06 06:58:18 -0300

    Agregar funcionalidad para vincular cuentas de MercadoPago en la página de métodos de pago. Se implementa un nuevo estado para mostrar un formulario de vinculación, incluyendo un botón que redirige al usuario a MercadoPago y un mensaje de alerta para mantener actualizada la información de la cuenta. Se mejora la navegación con un enlace de regreso a la página de ajustes.

--- src/app/dashboard/ajustes/metodos/page.tsx
+++ src/app/dashboard/ajustes/metodos/page.tsx
@@ -3,18 +3,96 @@ import { useGetMercadoPago } from '@/src/hooks/mercadopago/useGetMercadoPago'
 import { useGetUser } from '@/src/hooks/users/useGetUser'
 import { Alert, AlertIcon, Button, Circle, Divider, Flex, Spinner, Text } from '@chakra-ui/react'
 import { GrCheckmark } from 'react-icons/gr'
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '@/src/components/form/Input'
-import { BiArrowToRight } from 'react-icons/bi'
+import { BiArrowToLeft, BiArrowToRight, BiLinkExternal } from 'react-icons/bi'
+import { FaArrowLeft } from 'react-icons/fa'
+import Link from 'next/link'
 
 const MetodosPagoPage = () => {
   const { mercadopago, isLoading, refetch } = useGetMercadoPago()
   const { user, isLoading: isLoadingUser } = useGetUser()
+  const [linkNewAccount, setLinkNewAccount] = useState(false)
 
   if (isLoading || isLoadingUser) {
     return (
       <Flex justifyContent='center' alignItems='center' height='100%'>
         <Spinner size='lg' mt={4} color='primary' />
+      </Flex>
+    )
+  }
+
+  if (linkNewAccount) {
+    return (
+      <Flex
+        backgroundColor='gray.1000'
+        borderRadius='15px'
+        direction={'column'}
+        justifyContent='space-between'
+      >
+        <Flex justifyContent='space-between' alignItems={'center'} height={'58px'} mx={4}>
+          <Flex alignItems={'center'} gap={1}>
+            <FaArrowLeft
+              size={16}
+              style={{ marginRight: '5px' }}
+              onClick={() => setLinkNewAccount(false)}
+              cursor={'pointer'}
+            />
+            <Text fontWeight={700}>Métodos de pago</Text>
+          </Flex>
+        </Flex>
+        <Divider borderColor='components.divider' />
+        <Flex flex={1} mx={4} mt={4} mb={4} direction='column'>
+          <Flex justifyContent={'space-between'}>
+            <Flex direction={'column'} w='100%'>
+              <Text fontWeight={700}>Conecta tu cuenta de MercadoPago</Text>
+              <Text color={'gray.300'}>Vincula tu cuenta para empezar a recibir propinas</Text>
+              <Flex
+                backgroundColor='#262626'
+                borderRadius='15px'
+                direction={'column'}
+                py={2}
+                mt={4}
+                w={'100%'}
+              >
+                <Flex
+                  alignItems={'center'}
+                  justifyContent={'center'}
+                  gap={3}
+                  direction={'column'}
+                  mx={4}
+                >
+                  <Circle size='100px' bg='primary' color='white'>
+                    <BiLinkExternal size={50} />
+                  </Circle>
+                  <Text fontSize={'2xl'} fontWeight={700}>
+                    Vincular cuenta
+                  </Text>
+                  <Text fontSize={'xl'} fontWeight={600} w={'60%'} textAlign={'center'}>
+                    Serás redirigido a MercadoPago para completar el proceso de vinculación
+                  </Text>
+                  <Button mb={3} variant={'outline'} w={'100%'}>
+                    <BiLinkExternal size={16} style={{ marginRight: '5px' }} />
+                    Ir a MercadoPago
+                  </Button>
+                </Flex>
+              </Flex>
+              <Alert
+                status='warning'
+                borderRadius={'15px'}
+                backgroundColor={'transparent'}
+                border={'1px solid #D8CF38'}
+                color={'#D8CF38'}
+                mt={3}
+                fontWeight={600}
+              >
+                <AlertIcon color={'#D8CF38'} />
+                Mantén actualizada la información de tu cuenta para asegurar que recibas tus
+                propinas correctamente.
+              </Alert>
+            </Flex>
+          </Flex>
+        </Flex>
       </Flex>
     )
   }
@@ -27,7 +105,12 @@ const MetodosPagoPage = () => {
       justifyContent='space-between'
     >
       <Flex justifyContent='space-between' alignItems={'center'} height={'58px'} mx={4}>
-        <Text fontWeight={700}>Métodos de pago</Text>
+        <Flex alignItems={'center'} gap={1}>
+          <Link href={'/dashboard/ajustes'}>
+            <FaArrowLeft size={16} style={{ marginRight: '5px' }} cursor={'pointer'} />
+          </Link>
+          <Text fontWeight={700}>Métodos de pago</Text>
+        </Flex>
       </Flex>
       <Divider borderColor='components.divider' />
       <Flex flex={1} mx={4} mt={4} mb={4} direction='column'>
@@ -64,7 +147,7 @@ const MetodosPagoPage = () => {
               isDisabled
             />
           </Flex>
-          <Button mb={3} variant={'outline'}>
+          <Button mb={3} variant={'outline'} onClick={() => setLinkNewAccount(true)}>
             <BiArrowToRight size={16} style={{ marginRight: '5px' }} />
             Vincular nueva cuenta
           </Button>
